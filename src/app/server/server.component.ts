c51d40cdaba0fbbce88a833ab3d914f932c24466
brianbirir <brianbirir@gmail.com>
2019-11-24 11:22:52 +0300

    chore(code): eslint formatting

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html'
+  selector: "app-server",
+  templateUrl: "./server.component.html"
 })
 export class ServerComponent {
-    serverID: number = 10;
-    serverStatus: string = 'offline';
+  serverID: number = 10;
+  serverStatus: string = "offline";
 
-    getServerStatus() {
-        return this.serverStatus;
-    }
+  getServerStatus() {
+    return this.serverStatus;
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,21 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.scss']
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  styleUrls: ["./servers.component.scss"]
 })
 export class ServersComponent implements OnInit {
-
   allowNewServer = false;
-
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
